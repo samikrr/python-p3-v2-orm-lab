879a0618e2d5e8230ac4189ad8093225fd78ca35
Samuel <samuelkirori300@gmail.com>
2024-05-30 15:20:08 +0300

    Done

--- lib/employee.py
+++ lib/employee.py
@@ -1,6 +1,6 @@
-# lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
+import sqlite3
 
 class Employee:
 
@@ -9,13 +9,13 @@ class Employee:
 
     def __init__(self, name, job_title, department_id, id=None):
         self.id = id
-        self.name = name
+        self._name = name
         self.job_title = job_title
         self.department_id = department_id
 
     def __repr__(self):
         return (
-            f"<Employee {self.id}: {self.name}, {self.job_title}, " +
+            f"<Employee {self.id}: {self._name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
 
@@ -118,8 +118,9 @@ class Employee:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
-        # Delete the dictionary entry using id as the key
-        del type(self).all[self.id]
+        # Delete the dictionary entry using id as the key if it exists
+        if self.id in type(self).all:
+            del type(self).all[self.id]
 
         # Set the id to None
         self.id = None
@@ -134,19 +135,8 @@ class Employee:
     @classmethod
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
-
-        # Check the dictionary for  existing instance using the row's primary key
-        employee = cls.all.get(row[0])
-        if employee:
-            # ensure attributes match row values in case local instance was modified
-            employee.name = row[1]
-            employee.job_title = row[2]
-            employee.department_id = row[3]
-        else:
-            # not in dictionary, create new instance and add to dictionary
-            employee = cls(row[1], row[2], row[3])
-            employee.id = row[0]
-            cls.all[employee.id] = employee
+        employee = cls(row[1], row[2], row[3])
+        employee.id = row[0]
         return employee
 
     @classmethod
@@ -179,7 +169,7 @@ class Employee:
         sql = """
             SELECT *
             FROM employees
-            WHERE name is ?
+            WHERE name = ?
         """
 
         row = CURSOR.execute(sql, (name,)).fetchone()
@@ -187,4 +177,18 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review
+        try:
+            sql="""
+            SELECT * FROM review
+            WHERE Employee_id = ?
+            """
+            CURSOR.execute(sql, (self.id,))
+            rows = CURSOR.fetchall()
+            return [
+                Review.instance_from_db(row) for row in rows
+            ]
+        except sqlite3.OperationalError:
+            # Handle the case where the 'review' table doesn't exist
+            return []
+
--- lib/review.py
+++ lib/review.py
@@ -47,12 +47,20 @@ class Review:
         """ Insert a new row with the year, summary, and employee id values of the current Review object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
-        pass
+
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        
 
     @classmethod
     def create(cls, year, summary, employee_id):
         """ Initialize a new Review instance and save the object to the database. Return the new instance. """
-        pass
+        
    
     @classmethod
     def instance_from_db(cls, row):
@@ -79,4 +87,3 @@ class Review:
     def get_all(cls):
         """Return a list containing one Review instance per table row"""
         pass
-
